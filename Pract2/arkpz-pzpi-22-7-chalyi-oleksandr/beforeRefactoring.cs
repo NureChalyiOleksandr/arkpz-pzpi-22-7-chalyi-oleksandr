3feff5990f6a78918f894e7a690c72d7981a7859
Alex <oleksandr.chalyi1@nure.ua>
2025-01-01 20:35:53 +0200

    Add files via upload

---
+++ Pract2/arkpz-pzpi-22-7-chalyi-oleksandr/beforeRefactoring.cs
@@ -0,0 +1,46 @@
+﻿// До рефакторингу
+string input = Console.ReadLine();
+try
+{
+    int value = int.Parse(input);
+    Console.WriteLine($"Valid number: {value}");
+}
+catch (FormatException)
+{
+    Console.WriteLine("Invalid number format");
+}
+
+// До рефакторингу
+class Circle
+{
+    public double Radius { get; set; }
+
+    public double CalculateArea(double pi)
+    {
+        return pi * Radius * Radius;
+    }
+}
+
+
+
+
+// До рефакторингу
+class Animal
+{
+    public void Walk()
+    {
+        // Логіка для ходьби
+    }
+
+    public void Bark()
+    {
+        Console.WriteLine("Barking");
+    }
+
+    public void Meow()
+    {
+        Console.WriteLine("Meowing");
+    }
+}
+
+
